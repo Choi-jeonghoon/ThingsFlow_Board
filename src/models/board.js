bbfe43a3445fd8000de19ec1166126ceab210bba
최정훈 <jeongssi94@gmail.com>
2022-09-07 19:10:19 +0900

    refactor: 불필요한 코드 정리

--- src/models/board.js
+++ src/models/board.js
@@ -21,7 +21,6 @@ const postBoard = async (title, contents, lockpassword, weather) => {
 };
 
 const updateBoard = async (id, title, contents) => {
-  console.log(id, title, contents);
   return await prisma.board.update({
     where: { id: Number(id) },
     data: {
@@ -32,7 +31,6 @@ const updateBoard = async (id, title, contents) => {
 };
 
 const deleteBoard = async id => {
-  console.log(id);
   return await prisma.board.delete({
     where: { id: Number(id) },
   });
